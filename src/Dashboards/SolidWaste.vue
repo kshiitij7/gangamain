c7765ff42d7f68f7f40261ab1842bfd875533e68
kshiitij7 <kshiitijsrivastava20@gmail.com>
2025-01-01 20:56:12 +0530

    Update SolidWaste

--- src/Dashboards/SolidWaste.vue
+++ src/Dashboards/SolidWaste.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="SolidWaste">
         <v-main>
-            <div style="height: 86vh;">
+            <div style="height: 84vh;">
                 <MapComponent :dashLayers= "mapLayers" :center="[82.0662, 26.2648]" :zoom="10" :minZoom="5.5"/>
             </div>
     
